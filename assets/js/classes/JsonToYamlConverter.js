03e511e55d5ad31873540021aa0c284950c40671
abdulrahmanKanakri <aboodkanakri2000@gmail.com>
2020-05-19 12:22:58 +0300

    arrayOptionInJson

--- assets/js/classes/JsonToYamlConverter.js
+++ assets/js/classes/JsonToYamlConverter.js
@@ -17,8 +17,14 @@ export default class JsonToYamlConverter {
         str += this.spaces.substr(0, num) + 'properties: \n';
 
         for(let key in object) {
-            str += this.spaces.substr(0, num + 2) + key + ':' + '\n';
-            if(typeof object[key] != 'object' || object[key] == null) {
+            if(isNaN(key)) {
+                str += this.spaces.substr(0, num + 2) + key + ':' + '\n';
+            }
+            if(Array.isArray(object[key])) {
+                str += this.spaces.substr(0, num + 4) + 'type: array \n';
+                str += this.spaces.substr(0, num + 4) + 'items: \n';
+                str += this.parseJsonToYaml('', object[key], num + 6);
+            } else if(typeof object[key] != 'object' || object[key] == null) {
                 str += this.spaces.substr(0, num + 4) + 'type: ' + typeof object[key] + '\n';
                 str += this.spaces.substr(0, num + 4) + 'example: ' + object[key] + '\n';
             } else {
